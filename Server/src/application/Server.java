91dc04e8c4d790ee362f684b4372f4e143a3e9ea
Sergio <sergioandrade013@gmail.com>
2016-07-02 17:11:37 -0300

    Mudança das mensagens de feedback

--- Server/src/application/Server.java
+++ Server/src/application/Server.java
@@ -1,10 +1,11 @@
 package application;
 /**
- * Diego Wilde Rodrigues 					7239200
- * Matheus Mesquita Nascimento dos Santos 	8531459	
+ * Diego Wilde Rodrigues 				7239200
+ * Matheus Mesquita Nascimento dos Santos               8531459	
  * Sergio Andrade de Souza        			8531588
  */
 
+//
 import models.Candidato;
 import models.Vote;
 
@@ -30,7 +31,7 @@ public class Server {
 	private static List<Vote> mVotes;
 	
     public static void main(String[] args) throws IOException {
-    	String option;
+    	String option; //999 ou 888
     	
     	generateCandidatesList();
     	
@@ -39,11 +40,11 @@ public class Server {
     	ServerSocket mServerSocket = new ServerSocket(40011);
     	//192.168.0.28
         //mServerSocket.bind(new InetSocketAddress("192.168.0.100", 40011));
-        System.out.println(mServerSocket.getLocalSocketAddress());
+        System.out.println("Servidor iniciado");
     	
         try{
             while (true) {
-            	Socket optionSocket = mServerSocket.accept();
+            	Socket optionSocket = mServerSocket.accept(); 
             	option = getOption(optionSocket);
             	System.out.println(option);
             	switch (option) {
@@ -89,30 +90,34 @@ public class Server {
 		c4.setNome_candidato("Tiririca");
 		c4.setPartido("PR");
 		c4.setNum_votos(0);
+                
+                Candidato c5 = new Candidato();
+                c5.setCodigo_votacao(4);
+                c5.setNome_candidato("Branco");
+                c5.setPartido("Branco");
+                c5.setNum_votos(0);
 		
 		mCandidates.add(c1);
 		mCandidates.add(c2);
 		mCandidates.add(c3);
 		mCandidates.add(c4);
+                mCandidates.add(c5);
 	}
 
     
 	private static void sendCandidates(final Socket socket) throws IOException {
 		Gson gson = new Gson();
-        final JsonElement element = gson.toJsonTree(mCandidates, new TypeToken<List<Candidato>>() {}.getType());
+        final JsonElement element = gson.toJsonTree(mCandidates, new TypeToken<List<Candidato>>() {}.getType()); //Transformo a lista de candidatos em Json
 		
 		Thread thread = new Thread(){
             @Override
             public void run() {
                 try {
-                    OutputStreamWriter writer = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
-                    System.out.println("Server sent candidates list");
-                    
-                    //JSONArray jsonArray = new JSONArray(mCandidates);
-                    System.out.println(element.toString() + "\n");
-                    
-                    writer.write(element.toString()+"\n");
-                    writer.flush();
+                    OutputStreamWriter writer = new OutputStreamWriter(socket.getOutputStream(), "UTF-8"); //escrever no socket
+                    System.out.println("Servidor enviou lista de candidatos");
+                                        
+                    writer.write(element.toString()+"\n"); 
+                    writer.flush();                    
                 } catch (IOException e){
                     e.printStackTrace();
                 }finally{
@@ -133,37 +138,46 @@ public class Server {
     
     private static void receiveCandidate(final Socket socket) throws IOException {
     	FutureTask<Vote> thread = new FutureTask<Vote>( new Callable<Vote>() {
-			@Override
-			public Vote call() throws Exception {
-				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-                System.out.println("Client sent candidate");                   
-                System.out.println(reader.readLine());
+            @Override
+            public Vote call() throws Exception {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+                System.out.println("Cliente enviou candidato escolhido");   
               
                 Gson gson = new Gson();
-        		Vote vote = gson.fromJson(reader.readLine(), Vote.class);
+        	Vote vote = gson.fromJson(reader.readLine(), Vote.class);
                 
-        		socket.close();
+        	socket.close();
         		
                 return vote;
-	        }
+            }
     	});
         thread.run();
         
         try {
-			mVotes.add(thread.get());
-		} catch (InterruptedException | ExecutionException e) {
-			e.printStackTrace();
-		}
+            mVotes.add(thread.get()); //adicionar voto retornado na lista de votos
+            showTotalVotes();
+            
+	} catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+	}
+    }
+    
+    private static void showTotalVotes(){
+        for(Vote vote: mVotes){
+            System.out.println(vote.getCandidato().getNome_candidato()+": "+vote.getCandidato().getNum_votos());
+        }
+        
     }
     
     private static String getOption(final Socket socket) throws IOException {
-    	FutureTask<String> thread = new FutureTask<String>(new Callable<String>() {
+    	FutureTask<String> thread = new FutureTask<String>(new Callable<String>() //thread que vai me retornar uma string. Dentro da thread o Callable dá o retorno
+        {
     		String option;
     		BufferedReader mReader;
 			@Override
 			public String call() throws Exception {
 	            mReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-	            System.out.println("Client requested option");
+	            System.out.println("Cliente enviou opção");
 	            option = mReader.readLine();
 	        
 	        	socket.close();		           
