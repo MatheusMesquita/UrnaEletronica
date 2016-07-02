4e899019f102dae486bfcb830908ebb911f9cfb0
Matheus Mesquita <matheus@evolutechsolucoes.com.br>
2016-07-02 09:07:12 -0300

    sdv

--- app/src/main/java/br/com/redes/urnaeletronica/connection/Utils.java
+++ app/src/main/java/br/com/redes/urnaeletronica/connection/Utils.java
@@ -8,6 +8,6 @@ public class Utils {
     public static final String USER = "grupo11bsi";
     public static final String PASSWORD = "D1@M@NT3";
     public static final String SVADDRESS = "cosmos.lasdpc.icmc.usp.br";
-    //public static final String SV = "172.26.171.200";
+    //public static final String SV = "172.26.171.200";cosmos.lasdpc.icmc.usp.br
     public static final int PORT = 40011;
 }
